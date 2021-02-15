177e643938789b9245f828334d7263211cfa20a9
Kyrus1999 <71381995+Kyrus1999@users.noreply.github.com>
2021-02-15 15:42:33 +0100

    Update slapp.js
    updated date

--- slapp.js
+++ slapp.js
@@ -20,9 +20,9 @@ function setup() {
   title.style.color = "#ff0000";
   title.style.fontSize = "50px";
   tableSlots = [yearV, monthV, dayV, hourV, minuteV, secV];
-  let tyear = 2020;
-  let tmonth = 12;
-  let tday = 24;
+  let tyear = 2021;
+  let tmonth = 8;
+  let tday = 27;
   let thour = 20;
   let tminute = 0;
   let tsec = 0;
